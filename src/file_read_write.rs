f12d95fc218d34489570d31e888118d90da0e5d4
JustinTil <justin_til@hotmail.com>
2023-07-22 15:18:54 -0400

    Implemented the function to add a task to the file "toudou.txt".

--- src/file_read_write.rs
+++ src/file_read_write.rs
@@ -1,5 +1,4 @@
-
-
+use std::io::Write;
 
 pub fn read_lines(filename: &str) -> Vec<String> {
     std::fs::read_to_string(filename)
@@ -7,4 +6,14 @@ pub fn read_lines(filename: &str) -> Vec<String> {
         .lines()
         .map(String::from)
         .collect()
+}
+
+pub fn write_tasklist_to_file(tasklist: Vec<String>, filename: &str) -> Result<(), std::io::Error>{
+    let mut f  = std::fs::File::create(filename).unwrap();
+        
+    for task in tasklist {
+        f.write(format!("{task}\n").as_bytes())?;
+    }
+    
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
+
 pub mod parser;
 pub mod file_read_write;
 
-fn main() {
+fn main() -> std::io::Result<()>{
     #[allow(unused)]
     let cli_args = parser::get_args();
     
     let filename = "toudou.txt";
+    
     let mut tasklist = file_read_write::read_lines(filename);
     
-    dbg!(&tasklist);
+    // If no arguments: print the tasks to the screen
+    if cli_args.add_flag.is_none() && cli_args.check_flag.is_none() && cli_args.remove_flag.is_none() {
+        let mut index: i8 = 1;
+        
+        for task in &tasklist {
+            println!("{index} - {task}");
+            index += 1;
+        }
+    }
+    
+    // If requested to add a task
+    if cli_args.add_flag.is_some() {
+        tasklist.push(cli_args.add_flag.unwrap());
+        file_read_write::write_tasklist_to_file(tasklist, filename)?;
+    }
+    
+    Ok(())
 }
